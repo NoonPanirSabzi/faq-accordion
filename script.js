8658e63a439ab04c5304c70beb303336beb660d1
Amin Forouzan <amin.pqr@gmail.com>
2025-08-04 16:22:23 +0330

    feat(ui): add question's icon toggle logic

--- script.js
+++ script.js
@@ -4,24 +4,17 @@ let i = 0;
 let questions = [];
 
 const questionWrappers = document.querySelectorAll(".q-wrapper");
-// console.log(questionWrappers);
 
 questionWrappers.forEach((questionWrapper) => {
-  // to do : har answer, yek icon + open darad. mitavani icone close ra ham ezafe koni va dar ebteda hide koni
-  // sepas vaghti yek answer unhide shod, icon haye oun ham jabeja shavand. reference be icon ha ra haminja
-  // baid begiri va algorithm payin ra taghir dahi. ba tashakor!
- 
   obj = {
     index: i,
     quesion: questionWrapper.childNodes[1],
     answer: questionWrapper.childNodes[3],
+    icon: questionWrapper.childNodes[1].childNodes[3],
   };
   questions.push(obj);
   i += 1;
 });
-
-console.log(questions);
-
 
 questions.forEach((questionObj) => {
   questionObj.quesion.addEventListener("click", () => {
@@ -29,13 +22,21 @@ questions.forEach((questionObj) => {
       questionObj.answer.classList.remove("hide");
       activeQuestion = questionObj.index;
       isAnyActive = true;
+      // toggle icon
+      questionObj.icon.src = "assets/images/icon-minus.svg";
     } else {
       if (questionObj.index == activeQuestion) {
         questionObj.answer.classList.add("hide");
         isAnyActive = false;
-        activeQuestion = null;        
+        activeQuestion = null;
+        // toggle icon
+        questionObj.icon.src = "assets/images/icon-plus.svg";
       } else {
         questions[activeQuestion].answer.classList.add("hide");
+        // toggle icon for already open question
+        questions[activeQuestion].icon.src = "assets/images/icon-plus.svg";
+        // toggle icon for the question we want to open now
+        questionObj.icon.src = "assets/images/icon-minus.svg";
         questionObj.answer.classList.remove("hide");
         activeQuestion = questionObj.index;
       }
