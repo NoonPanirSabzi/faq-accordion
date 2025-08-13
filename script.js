0bce5b03182c367af1395bc26550ac0e43dd4d7d
Amin Forouzan <amin.pqr@gmail.com>
2025-08-13 20:58:30 +0330

    refactor(js): now uses more reliable method

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
+const questions = [...document.querySelectorAll(".q-container")].map(
+  (container) => ({
+    container,
+    question: container.querySelector(".question"),
+    answer: container.querySelector(".answer"),
+    icon: container.querySelector(".toggle-icon"),
+  })
+);
+
 let activeQuestion = null;
-let isAnyActive = false;
-let i = 0;
-let questions = [];
 
-const questionWrappers = document.querySelectorAll(".q-wrapper");
+const toggleAnswer = (questionObj, show) => {
+  if (show) {
+    questionObj.answer.classList.remove("hide");
+    questionObj.icon.src = "assets/images/icon-minus.svg";
+  } else {
+    questionObj.answer.classList.add("hide");
+    questionObj.icon.src = "assets/images/icon-plus.svg";
+  }
+};
 
-questionWrappers.forEach((questionWrapper) => {
-  obj = {
-    index: i,
-    quesion: questionWrapper.childNodes[1],
-    answer: questionWrapper.childNodes[3],
-    icon: questionWrapper.childNodes[1].childNodes[3],
-  };
-  questions.push(obj);
-  i += 1;
-});
-
-questions.forEach((questionObj) => {
-  questionObj.quesion.addEventListener("click", () => {
-    if (isAnyActive == false) {
-      questionObj.answer.classList.remove("hide");
-      activeQuestion = questionObj.index;
-      isAnyActive = true;
-      // toggle icon
-      questionObj.icon.src = "assets/images/icon-minus.svg";
+questions.forEach((q, index) => {
+  q.question.addEventListener("click", () => {
+    if (activeQuestion === index) {
+      toggleAnswer(q, false);
+      activeQuestion = null;
     } else {
-      if (questionObj.index == activeQuestion) {
-        questionObj.answer.classList.add("hide");
-        isAnyActive = false;
-        activeQuestion = null;
-        // toggle icon
-        questionObj.icon.src = "assets/images/icon-plus.svg";
-      } else {
-        questions[activeQuestion].answer.classList.add("hide");
-        // toggle icon for already open question
-        questions[activeQuestion].icon.src = "assets/images/icon-plus.svg";
-        // toggle icon for the question we want to open now
-        questionObj.icon.src = "assets/images/icon-minus.svg";
-        questionObj.answer.classList.remove("hide");
-        activeQuestion = questionObj.index;
+      if (activeQuestion !== null) {
+        toggleAnswer(questions[activeQuestion], false);
       }
+      toggleAnswer(q, true);
+      activeQuestion = index;
     }
   });
 });
